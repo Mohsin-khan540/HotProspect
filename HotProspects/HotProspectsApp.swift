dfb45e8e4cb378699ad086ad21e56d2042473243
Mohsin-khan540 <arhamk4555@gmail.com>
2025-11-09 22:15:07 +0500

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Mohsin khan on 09/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
